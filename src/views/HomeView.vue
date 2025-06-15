8c7b2a449bd9d68624aa13b2d83b891a480e0d26
Ádám Z. Kövér <adamzkover@gmail.com>
2025-06-15 15:55:13 +0200

    Request resources for patient

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,51 @@
 import { ref, onMounted } from 'vue'
 import FHIR from 'fhirclient'
 
-const output = ref('Loading FHIR data...')
+const patientId = ref('Loading...')
+const patientData = ref('Loading...')
+const userData = ref('Loading...')
+const medications = ref('Loading...')
 
 onMounted(() => {
   FHIR.oauth2.ready().then(client => {
-    // Example: fetch the current Patient resource
-    client.request('Patient').then(res => {
-      output.value = JSON.stringify(res, null, 2)
+    // 1. Show client.patient.id
+    patientId.value = client.patient.id || 'No patient context'
+
+    // 2. Show client.patient.read() JSON string
+    client.patient.read().then(res => {
+      patientData.value = JSON.stringify(res, null, 2)
     }).catch(err => {
-      output.value = 'FHIR request error: ' + err
+      patientData.value = 'Error: ' + err
     })
+
+    // 3. Show client.user.read() JSON string
+    if (client.user && client.user.read) {
+      client.user.read().then(res => {
+        userData.value = JSON.stringify(res, null, 2)
+      }).catch(err => {
+        userData.value = 'Error: ' + err
+      })
+    } else {
+      userData.value = 'No user context'
+    }
+
+    // 4. Fetch MedicationStatement for the patient
+    if (client.patient && client.patient.id) {
+      client.request(`MedicationStatement?subject=Patient/${client.patient.id}`)
+        .then(res => {
+          medications.value = JSON.stringify(res, null, 2)
+        })
+        .catch(err => {
+          medications.value = 'Error: ' + err
+        })
+    } else {
+      medications.value = 'No patient context for medications'
+    }
   }).catch(err => {
-    output.value = 'SMART client error: ' + err
+    patientId.value = 'SMART client error: ' + err
+    patientData.value = 'SMART client error: ' + err
+    userData.value = 'SMART client error: ' + err
+    medications.value = 'SMART client error: ' + err
   })
 })
 </script>
@@ -21,8 +54,20 @@ onMounted(() => {
 <template>
   <main>
     <section>
-      <h2>FHIR Server Data</h2>
-      <pre>{{ output }}</pre>
+      <h2>Patient ID</h2>
+      <pre>{{ patientId }}</pre>
+    </section>
+    <section>
+      <h2>Patient Resource</h2>
+      <pre>{{ patientData }}</pre>
+    </section>
+    <section>
+      <h2>User Resource</h2>
+      <pre>{{ userData }}</pre>
+    </section>
+    <section>
+      <h2>Patient Medications (MedicationStatement)</h2>
+      <pre>{{ medications }}</pre>
     </section>
   </main>
 </template>
